790232745b644298da55c41e7d58905ea93a17c5
maarselo <marce@decaos.com>
2025-04-20 17:16:18 +0200

    Add create struct for map and fucntion aux to count lines to malloc

--- create.c
+++ create.c
@@ -12,15 +12,42 @@
 
 #include "so_long.h"
 
-int	ft_create_map(char *file, t_map *map)
+static int	ft_count_line(int fd)
 {
-	int	total_lines;
+	int			lines;
+	char	*line;
 
-	total_lines = ft_count_line(file);
-	map.rows = total_lines;
-	map.columns = -1;
-	map.map = (char **)malloc(sizeof(char *) * total_lines);
-	if (!map.map)
-		return (1);
-	return (0);
+	lines = 0;
+	line = get_next_line(fd);
+	while(line)
+	{
+		lines++;
+		free(line);
+		line = get_next_line(fd);
+	}
+	return (lines);
+}
+
+t_map	*ft_create_map(char *file)
+{
+	int			total_lines;
+	t_map	*map;
+	int			fd;
+
+	fd = open(file, O_RDONLY);
+	total_lines = ft_count_line(fd);
+	map = (t_map *)malloc(sizeof(t_map));
+	if (!total_lines || !map)
+		return (NULL);
+	map->rows = total_lines;
+	map->columns = 0;
+	map->map = (char **)malloc(sizeof(char *) * (total_lines + 1));
+	if (!map->map)
+	{
+		free(map);
+		return (NULL);
+	}
+	map->map[total_lines + 1] = NULL;
+	close(fd);
+	return (map);
 }
