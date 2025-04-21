a4039e60cdc1970acfc87a897a035be8ee57a579
maarselo <marce@decaos.com>
2025-04-21 16:47:19 +0200

    Add create.c to create strcut for map with rows and columns, and fill with the content of file

--- create.c
+++ create.c
@@ -12,43 +12,101 @@
 
 #include "so_long.h"
 
-static int	ft_count_line(char *file)
+static int	ft_get_rows(char *file)
 {
-	int			fd = open(file, O_RDONLY);
-	printf("%s", get_next_line(fd));
-	/*int			lines;
-	char	*line;
+	int			fd;
+	int			lines;
+	char		*line;
 
 	lines = 0;
-	while((line = get_next_line(fd)))
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
 	{
 		lines++;
-		//free(line);
+		free(line);
+		line = get_next_line(fd);
 	}
-	return (lines);*/
 	close(fd);
-	return (0);
+	return (lines);
+}
+
+static int	ft_get_columns(char *file)
+{
+	int			fd;
+	char		*line;
+	int			columns;
+
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	columns = ft_check_len(line);
+	while (line)
+	{
+		if (columns != ft_check_len(line))
+			return (0);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (columns);
+}
+
+static void	ft_fill_lines(char *file, t_map *map)
+{
+	int			fd;
+	int			index;
+	char		*line;
+
+	index = 0;
+	fd = open(file, O_RDONLY);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (line[ft_strlen(line) - 1] == '\n')
+			line[ft_strlen(line) - 1] = '\0';
+		map->map[index++] = ft_strdup(line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+}
+
+static void	ft_check_lines(t_map *map)
+{
+	int		i;
+
+	i = -1;
+	while (++i < map->rows)
+	{
+		if (!map->map[i])
+		{
+			ft_free_struct_map(map);
+			ft_error();
+		}
+	}
 }
 
 t_map	*ft_create_map(char *file)
 {
 	int			total_lines;
-	t_map		*map = NULL;
+	int			columns;
+	t_map		*map;
 
-	total_lines = ft_count_line(file);
-	printf("you:%dnigg", total_lines);
-	//map = (t_map *)malloc(sizeof(t_map));
-	/*if (!map)
-		return (NULL);
+	map = (t_map *)malloc(sizeof(t_map));
+	if (!map)
+		ft_error();
+	total_lines = ft_get_rows(file);
 	map->rows = total_lines;
-	map->columns = 0;
-	map->map = (char **)malloc(sizeof(char *) * (total_lines + 1));
+	columns = ft_get_columns(file);
+	map->columns = columns;
+	map->map = malloc(sizeof(char *) * (total_lines + 1));
 	if (!map->map)
 	{
 		free(map);
-		return (NULL);
+		ft_error();
 	}
 	map->map[total_lines + 1] = NULL;
-	close(fd);*/
+	ft_fill_lines(file, map);
+	ft_check_lines(map);
 	return (map);
 }
