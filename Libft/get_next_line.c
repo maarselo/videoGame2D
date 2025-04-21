6d567f3bcf4ce4ead28e7d6b04f183b410082f00
maarselo <marce@decaos.com>
2025-04-21 16:49:42 +0200

    Add for not confuse in the buffer, finish the buffer to value NULL

--- Libft/get_next_line.c
+++ Libft/get_next_line.c
@@ -14,92 +14,98 @@
 
 #include "libft.h"
 
-static void	ft_buffjoin(char **buffer, char *tmp_buffer)
-{
-	char	*tmp;
-
-	if (!buffer || !tmp_buffer)
-		return ;
-	tmp = *buffer;
-	*buffer = ft_strjoin(*buffer, tmp_buffer);
-	free(tmp);
-}
-
-static char	*ft_getline(char *buffer)
-{
-	size_t	i;
-
-	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	if (buffer[i] == '\n')
-		i++;
-	return (ft_substr(buffer, 0, i));
-}
-
-static void	ft_nextline(char **buffer)
-{
-	char	*tmp;
-	size_t	i;
-	size_t	len;
-
-	if (!*buffer)
-		return ;
-	i = 0;
-	tmp = *buffer;
-	while ((*buffer)[i] && (*buffer)[i] != '\n')
-		i++;
-	if ((*buffer)[i] == '\n')
-		i++;
-	len = ft_strlen(*buffer);
-	*buffer = ft_substr(*buffer, i, len - i);
-	free(tmp);
-}
-
-static void	ft_read(int fd, char **buffer)
-{
-	ssize_t	bytes_read;
-	char	*tmp_buffer;
-
-	tmp_buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!tmp_buffer)
-		return ;
-	bytes_read = 1;
-	while (bytes_read > 0 && !ft_strchr(tmp_buffer, '\n'))
+	static void	ft_buffjoin(char **buffer, char *tmp_buffer)
 	{
-		bytes_read = read(fd, tmp_buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-		{
-			free(tmp_buffer);
+		char	*tmp;
+
+		if (!buffer || !tmp_buffer)
 			return ;
-		}
-		tmp_buffer[bytes_read] = '\0';
-		ft_buffjoin(buffer, tmp_buffer);
+		tmp = *buffer;
+		*buffer = ft_strjoin(*buffer, tmp_buffer);
 		if (!*buffer)
 		{
-			free(tmp_buffer);
+			*buffer = tmp; // o puedes liberar tmp también si lo necesitas
 			return ;
 		}
+		free(tmp);
 	}
-	free(tmp_buffer);
-}
 
-char	*get_next_line(int fd)
-{
-	static char	*buffer;
-	char		*line;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	ft_read(fd, &buffer);
-	if (!buffer || !*buffer)
+	static char	*ft_getline(char *buffer)
 	{
-		free(buffer);
-		return (NULL);
+		size_t	i;
+
+		i = 0;
+		while (buffer[i] && buffer[i] != '\n')
+			i++;
+		if (buffer[i] == '\n')
+			i++;
+		return (ft_substr(buffer, 0, i));
 	}
-	line = ft_getline(buffer);
-	if (!line)
-		free(buffer);
-	ft_nextline(&buffer);
-	return (line);
-}
+
+	static void	ft_nextline(char **buffer)
+	{
+		char	*tmp;
+		size_t	i;
+		size_t	len;
+
+		if (!*buffer)
+			return ;
+		i = 0;
+		tmp = *buffer;
+		while ((*buffer)[i] && (*buffer)[i] != '\n')
+			i++;
+		if ((*buffer)[i] == '\n')
+			i++;
+		len = ft_strlen(*buffer);
+		*buffer = ft_substr(*buffer, i, len - i);
+		free(tmp);
+	}
+
+	static void	ft_read(int fd, char **buffer)
+	{
+		ssize_t	bytes_read;
+		char	*tmp_buffer;
+
+		tmp_buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!tmp_buffer)
+			return ;
+		bytes_read = 1;
+		while (bytes_read > 0 && !ft_strchr(tmp_buffer, '\n'))
+		{
+			bytes_read = read(fd, tmp_buffer, BUFFER_SIZE);
+			if (bytes_read < 0)
+			{
+				free(tmp_buffer);
+				return ;
+			}
+			tmp_buffer[bytes_read] = '\0';
+			ft_buffjoin(buffer, tmp_buffer);
+			if (!*buffer)
+			{
+				free(tmp_buffer);
+				return ;
+			}
+		}
+		free(tmp_buffer);
+	}
+
+	char	*get_next_line(int fd)
+	{
+		static char	*buffer;
+		char		*line;
+
+		if (fd < 0 || BUFFER_SIZE <= 0)
+			return (NULL);
+		ft_read(fd, &buffer);
+		if (!buffer || !*buffer)
+		{
+			free(buffer);
+			buffer = NULL;
+			return (NULL);
+		}
+		line = ft_getline(buffer);
+		if (!line)
+			free(buffer);
+		ft_nextline(&buffer);
+		return (line);
+	}
