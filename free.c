2dbf10fbae076ba8b1ad3a390ac4325d2f14065a
maarselo <marce@decaos.com>
2025-04-29 10:27:39 +0200

    check in struct free if it exits

--- free.c
+++ free.c
@@ -16,13 +16,18 @@ void	ft_free_struct_map(t_map *map)
 {
 	int	i;
 
-	i = 0;
-	while (map->map[i])
+	if (!map)
+		return ;
+	if (map->map)
 	{
-		free(map->map[i]);
-		i++;
+		i = 0;
+		while (map->map[i])
+		{
+			free(map->map[i]);
+			i++;
+		}
+		free(map->map);
 	}
-	free(map->map);
 	free(map);
 }
 
