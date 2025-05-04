e0121747b49c7acc7c5bf1731a39076b12e57753
maarselo <marce@decaos.com>
2025-05-04 21:08:56 +0200

    Add all main functions and the animated the enemies

--- bonus/game_bonus.c
+++ bonus/game_bonus.c
@@ -12,21 +12,6 @@
 
 #include "so_long_bonus.h"
 
-static void ft_select_tile(int i, int j, t_game *game)
-{
-	if (game->map->map[i][j] == '1')
-		mlx_image_to_window(game->mlx, game->wall, j * PIXELS, i * PIXELS);
-	if (game->map->map[i][j] == 'P')
-		mlx_image_to_window(game->mlx, game->character->initial, j * PIXELS, i * PIXELS);
-	if (game->map->map[i][j] == 'E')
-		mlx_image_to_window(game->mlx, game->exit->close, j * PIXELS, i * PIXELS);
-	if (game->map->map[i][j] == 'C')
-		mlx_image_to_window(game->mlx, game->collectionable, j * PIXELS, i * PIXELS);
-	if (game->map->map[i][j] == 'A')
-		mlx_image_to_window(game->mlx, game->enemy_frames[0], j * PIXELS, i * PIXELS);
-
-}
-
 static void	ft_first_draw(t_game *game)
 {
 	int	i;
@@ -44,10 +29,71 @@ static void	ft_first_draw(t_game *game)
 	}
 }
 
+static void	ft_key_hook(mlx_key_data_t key, void *game)
+{
+	t_game	*g;
+
+	g = (t_game *)game;
+	if ((key.key == MLX_KEY_UP || key.key == MLX_KEY_LEFT
+			|| key.key == MLX_KEY_RIGHT || key.key == MLX_KEY_DOWN)
+		&& key.action == MLX_PRESS)
+	{
+		if (key.key == MLX_KEY_UP && key.action == MLX_PRESS)
+			g->moves += ft_move_up(game);
+		if (key.key == MLX_KEY_LEFT && key.action == MLX_PRESS)
+			g->moves += ft_move_left(game);
+		if (key.key == MLX_KEY_RIGHT && key.action == MLX_PRESS)
+			g->moves += ft_move_right(game);
+		if (key.key == MLX_KEY_DOWN && key.action == MLX_PRESS)
+			g->moves += ft_move_down(game);
+		ft_printf("You moved %d times — The treasure awaits!\n", g->moves);
+	}
+	if (key.key == MLX_KEY_ESCAPE && key.action == MLX_PRESS)
+		ft_close(game);
+}
+
+static void	ft_animate_enemies(void *game)
+{
+	int		i;
+	static int		timer;
+	t_game	*g;
+	t_enemy	*enemy_tmp;
+
+	g = (t_game *)game;
+	timer++;
+	if (timer > 15)
+	{
+		i = 0;
+		timer = 0;
+		while (i < g->map->enemies)
+		{
+			enemy_tmp = &g->enemies[i];
+			if (g->floor)
+				mlx_image_to_window(g->mlx, g->floor, enemy_tmp->y * PIXELS, enemy_tmp->x * PIXELS);
+			mlx_image_to_window(g->mlx, g->enemy_frames[enemy_tmp->current_frame], enemy_tmp->y * PIXELS, enemy_tmp->x * PIXELS);
+			enemy_tmp->current_frame++;
+			if (enemy_tmp->current_frame >= 4)
+				enemy_tmp->current_frame = 0;
+			i++;
+		}
+	}
+}
+
+static void	ft_close_x(void *game)
+{
+	t_game	*g;
+
+	g = (t_game *)game;
+	ft_printf("\033[1;31mGame closed. See you next time! 👋\033[0m\n");
+	ft_free_struct_game(g);
+	exit(0);
+}
+
 void	ft_init_game(t_game *game)
 {
 	ft_first_draw(game);
 	mlx_key_hook(game->mlx, ft_key_hook, game);
+	mlx_loop_hook(game->mlx, &ft_animate_enemies, game);
 	mlx_close_hook(game->mlx, ft_close_x, game);
 	mlx_loop(game->mlx);
 }
