09dfceb078ce54a25d608517aaa1921db805eba6
maarselo <marce@decaos.com>
2025-05-04 21:08:31 +0200

    Add for animated moves the struct and free

--- bonus/errorFree_bonus.c
+++ bonus/errorFree_bonus.c
@@ -26,49 +26,6 @@ void	ft_error_free_game(t_game *game)
 	ft_error();
 }
 
-void	ft_error_character(t_character *character, t_game *game)
-{
-	if (!character)
-		return ;
-	if (character->initial)
-		mlx_delete_image(game->mlx, character->initial);
-	if (character->up)
-		mlx_delete_image(game->mlx, character->up);
-	if (character->left)
-		mlx_delete_image(game->mlx, character->left);
-	if (character->right)
-		mlx_delete_image(game->mlx, character->right);
-	if (character->down)
-		mlx_delete_image(game->mlx, character->down);
-	if (game)
-		ft_free_struct_game(game);
-	free(character);
-	character = NULL;
-	ft_error_message("\033[1;33m🔍 Can't find characters PNG\033[0m\n");
-}
-
-void	ft_error_exit(t_exit *exit, t_game *game)
-{
-	if (!exit)
-		return ;
-	if (exit->close)
-		mlx_delete_image(game->mlx, exit->close);
-	if (exit->open)
-		mlx_delete_image(game->mlx, exit->open);
-	if (game)
-		ft_free_struct_game(game);
-	free(exit);
-	exit = NULL;
-	ft_error_message("\033[1;33m🔍 Can't find exits PNG\033[0m\n");
-}
-
-void	ft_error_enemies(t_game *game)
-{
-	if (game)
-		ft_free_struct_game(game);
-	ft_error_message("\033[1;33m🔍 Can't find enemies PNG\033[0m\n");
-}
-
 void	ft_free_game_message(t_game *game, char *message)
 {
 	if (game)
