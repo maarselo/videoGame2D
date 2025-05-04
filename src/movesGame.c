bf60097281806d78e9290cff05fb268343010141
maarselo <marce@decaos.com>
2025-05-04 04:36:35 +0200

    Rename

--- src/movesGame.c
+++ src/movesGame.c
@@ -50,14 +50,22 @@ static void	ft_move_down(t_game *game)
 
 void	ft_key_hook(mlx_key_data_t key, void *game)
 {
-	if (key.key == MLX_KEY_UP && key.action == MLX_PRESS)
-		ft_move_up(game);
-	if (key.key == MLX_KEY_LEFT && key.action == MLX_PRESS)
-		ft_move_left(game);
-	if (key.key == MLX_KEY_RIGHT && key.action == MLX_PRESS)
-		ft_move_right(game);
-	if (key.key == MLX_KEY_DOWN && key.action == MLX_PRESS)
-		ft_move_down(game);
+	t_game	*g;
+
+	g = (t_game *)game;
+	if ((key.key == MLX_KEY_UP || key.key == MLX_KEY_LEFT || key.key == MLX_KEY_RIGHT || key.key == MLX_KEY_DOWN)
+		&& key.action == MLX_PRESS)
+	{
+		if (key.key == MLX_KEY_UP && key.action == MLX_PRESS)
+			ft_move_up(game);
+		if (key.key == MLX_KEY_LEFT && key.action == MLX_PRESS)
+			ft_move_left(game);
+		if (key.key == MLX_KEY_RIGHT && key.action == MLX_PRESS)
+			ft_move_right(game);
+		if (key.key == MLX_KEY_DOWN && key.action == MLX_PRESS)
+			ft_move_down(game);
+		ft_printf("You moved %d times — The treasure awaits!\n", ++g->moves);
+	}
 	if (key.key == MLX_KEY_ESCAPE && key.action == MLX_PRESS)
 		ft_close(game);
 }
